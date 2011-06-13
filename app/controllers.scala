9e6df90995d933d23cf3218e757807ed80bdb6f4
Benoît Courtine <benoit.courtine@gmail.com>
2011-06-13 09:50:26 +0200

    Création d'un formulaire affichant une grille de Sudoku, et du contrôleur associé.

--- app/controllers.scala
+++ app/controllers.scala
@@ -1,14 +1,34 @@
 package controllers
 
 import play._
-import play.mvc._
+import mvc._
 
+/**
+ * Contrôleur de la page d'accueil par défaut de l'application.
+ */
 object Application extends Controller {
-    
+
     import views.Application._
-    
+
     def index = {
         html.index("Your Scala application is ready!")
     }
-    
+}
+
+/**
+ * Contrôleur de la grille de Sudoku.
+ */
+object Sudoku extends Controller {
+
+    import views.Sudoku._
+
+    /** Affichage d'une grille de Sudoku vide. */
+    def solver = {
+        html.show("Resolver de Sudoku")
+    }
+
+    /** Affiche la solution du Sudoku fourni. */
+    def solution = {
+        html.show("Solution du Sudoku")
+    }
 }
